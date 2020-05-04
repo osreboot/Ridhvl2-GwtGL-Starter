a6aa7b517421b90dfd0d7c619931401bb72c4996
osreboot <calvin.16.weaver@gmail.com>
2020-05-03 21:48:38 -0700

    Initial commit

---
+++ src/com/osreboot/r2gwtglstarter/client/Ridhvl2_GwtGL_Starter.java
@@ -0,0 +1,48 @@
+package com.osreboot.r2gwtglstarter.client;
+
+import com.google.gwt.canvas.client.Canvas;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.googlecode.gwtgl.binding.WebGLRenderingContext;
+
+public class Ridhvl2_GwtGL_Starter implements EntryPoint{
+
+	public void onModuleLoad(){
+		try{
+			RootPanel.get("ridhvl2_gwtgl_starter").add(new Ridhvl2_GwtGL_Starter().getWidget());
+		}catch(Exception e){
+			Window.alert("Fatal Error: " + e.getMessage());
+		}
+	}
+
+	protected final Canvas canvas;
+	protected final WebGLRenderingContext glContext;
+
+	public Ridhvl2_GwtGL_Starter(){
+		canvas = Canvas.createIfSupported();
+		canvas.setCoordinateSpaceWidth(512);
+		canvas.setCoordinateSpaceHeight(512);
+		glContext = (WebGLRenderingContext)canvas.getContext("webgl");
+		if(glContext == null){
+			Window.alert("Fatal Error: WebGL not supported!");
+		}else{
+			glContext.viewport(0, 0, 512, 512);
+			glContext.clearColor(1f, 0f, 1f, 1f);
+			Timer timer = new Timer(){
+				@Override
+				public void run(){
+					glContext.clear(WebGLRenderingContext.COLOR_BUFFER_BIT);
+				}
+			};
+			timer.scheduleRepeating(20);
+		}
+	}
+
+	public Widget getWidget(){
+		return canvas;
+	}
+
+}
